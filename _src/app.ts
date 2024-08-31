cd521adcb3a0b3761c8ceebcbcb64423e35af732
Ali Goudarzi <me74242@gmail.com>
2024-08-31 16:19:22 +0330

    #

--- _src/app.ts
+++ _src/app.ts
@@ -14,6 +14,15 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/calendar", CalendarRoutes);
 
 app.use("*", (_, res) => {
